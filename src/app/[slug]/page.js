2996d2422e05f7d9f7c403ca875f3c5370110518
Julian Lechner <jll38@njit.edu>
2023-08-07 17:21:11 +0200

    GET request, db record

--- src/app/[slug]/page.js
+++ src/app/[slug]/page.js
@@ -1,24 +1,30 @@
 "use client";
 import { useEffect, useState } from "react";
+
 export default function Redirect({ params }) {
-    const [data, setData] = useState(null)
-    try {
-        console.log("Slug is " + params.slug)
-        const res = fetch(`/api/match-url?slug=${params.slug}`);
+  const [data, setData] = useState(null);
+  
+  useEffect(() => {
+    const slug = params.slug;
+
+    fetch(`/api/match-url?slug=${slug}`)
+      .then(res => {
         if (!res.ok) {
           throw new Error(res.statusText);
         }
-        const data = res.json();
-        return data;
-      } catch (error) {
-        console.error("Server did a fucky wucky");
-        // handle the error according to your application logic
-      }
+        return res.json();
+      })
+      .then(data => {setData(data);
+      console.log(data)})
+      .catch(error => {
+        console.error("Error:", error);
+      });
+  }, [params.slug]);
 
   return (
     <div className="h-screen">
       <div>Slug: {params.slug}</div>
-      <div>Redirecting to: {data} </div>
+      <div>Redirecting to: {data?.url || 'Loading...'} </div>
     </div>
   );
 }
--- src/app/api/match-url/route.js
+++ src/app/api/match-url/route.js
@@ -1,40 +1,39 @@
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-import { Redirect } from "next";
+import qs from "qs";
 
 export async function GET(request) {
+  let originalURL;
   const prisma = new PrismaClient();
-  const slug = request.query;
-  console.log("Slug " + slug);
-  try {
-    //TODO: Handle GET request and check DB
-    const shortURLRecord = prisma.liteUrl.findFirst({
-      where: {
-        shortURL: slug,
-      },
-    });
-    if (!shortURLRecord) {
-      return NextResponse.json(home);
-    } else {
-      const originalURL = (await shortURLRecord).originalURL;
-      return NextResponse.json({ originalURL });
-    }
-  } catch (e) {
-    console.log("Error in fetching data", error);
-    return NextResponse.error({ status: 500 });
-  } finally {
-    await prisma.$disconnect(); // Disconnect prisma client
+  const url = new URL(request.url);
+  const searchParams = new URLSearchParams(url.search);
+  const slug = searchParams.get("slug");
+  
+  const urlRecord = await prisma.liteUrl.findFirst({
+    where: {
+      shortURL: "http://localhost:3000/" + slug,
+    },
+  });
+  if (urlRecord){
+    originalURL = urlRecord.originalURL;
+  } else {
+    return new NextResponse(JSON.stringify({error: "No matching url found"}), {
+      status: 404,
+    })
   }
-
-  return NextResponse.json({ body });
+  // Stringify the object and set it as the body parameter
+  return new NextResponse(JSON.stringify({ url: originalURL, message: "MATCH FOUND" }), {
+    status: 200,
+  });
 }
 
-function doesExistJSON(url){ //Returns JSON object with information pertaining to whether the url exists in any database and which db it belongs to.
+function doesExistJSON(url) {
+  //Returns JSON object with information pertaining to whether the url exists in any database and which db it belongs to.
   //TODO: This function
   const exists = false; //False if doesn't exist in either database.
   const isPremium = false; //False if found in in liteURL database, True if not in liteURL but in premiumURL database
   return {
     exists: exists,
-    isPremium: isPremium
-  }
+    isPremium: isPremium,
+  };
 }
