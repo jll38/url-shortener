a19215397f32563839396e0f4eb1bf9d057ab87e
Julian Lechner <jll38@njit.edu>
2023-08-08 20:22:28 +0200

    features page

--- src/app/features/page.js
+++ src/app/features/page.js
@@ -10,23 +10,52 @@ export default function Features() {
         >
           <div className="w-full h-full flex flex-col items-center justify-center uppercase font-black text-[4em] text-center ">
             <div className="text-delft-blue">The Tools You Need</div>
-            <div className="text-moonstone">All in One Place.</div>
+            <div className="text-cyan-500">All in One Place.</div>
           </div>
         </section>
         <section
           name="features"
-          className="bg-white/90 w-[300px] sm:w-[600px] h-[600px] flex rounded-lg"
+          className=" w-[300px] sm:w-[600px] h-[566px] flex rounded-lg"
         >
+          <div className="relative">
+            <div className="absolute h-[600px] w-[600px] z-0">
+              <ul className="w-full h-full relative top-[38px] px-2">
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+                <li className="w-full h-[1.5em]"></li>
+                <li className="bg-payne-gray/25 w-full h-[1.5em]"></li>
+              </ul>
+            </div>
+          </div>
           <div
             name="features-list"
-            className=" border-solid  w-[300px] sm:w-1/2 p-2 border-r-4 border-delft-blue"
+            className=" border-solid  w-[300px] sm:w-1/2 p-2 border-r-4 border-delft-blue relative"
           >
             <div className="font-semibold text-xl text-delft-blue">
               Features
             </div>
             <div className="h-[2px] bg-payne-gray/75 rounded-lg"></div>
-            <ul className="text-payne-gray">
-              <li>Basic URL Shortening</li>
+
+            <ul className="text-payne-gray z-30 ">
+              <li className="">Basic URL Shortening</li>
               <li>Basic Analytics</li>
               <li>Limited Link History</li>
               <li>Basic Redirects</li>
@@ -49,7 +78,10 @@ export default function Features() {
               <li>Integration with Other Platforms</li>
             </ul>
           </div>
-          <div name="lite-features" className=" w-[300px] sm:w-1/4 p-2">
+          <div
+            name="lite-features"
+            className=" w-[300px] sm:w-1/4 p-2 relative z-30"
+          >
             <div className="font-semibold text-xl text-delft-blue">Lite</div>
             <div className="h-[2px] bg-payne-gray/75 rounded-lg"></div>
             <ul className="text-payne-gray text-center w-full">
@@ -122,7 +154,10 @@ export default function Features() {
               </li>
             </ul>
           </div>
-          <div name="lite-features" className=" w-[300px] sm:w-1/4 p-2">
+          <div
+            name="lite-features"
+            className=" w-[300px] sm:w-1/4 p-2 relative z-30"
+          >
             <div className="font-semibold text-xl text-delft-blue">Premium</div>
             <div className="h-[2px] bg-payne-gray/75 rounded-lg"></div>
             <ul className="text-payne-gray text-center w-full">
