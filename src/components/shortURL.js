e9784100e7159dbe4cb69a850a1b0b4e5f48dac7
Julian Lechner <jll38@njit.edu>
2023-08-11 17:01:59 +0200

    url-anim

--- src/components/shortURL.js
+++ src/components/shortURL.js
@@ -1,6 +1,11 @@
+import { motion } from "framer-motion";
+
 export default function ShortURL({ children }) {
   return (
-    <>
+    <motion.div
+    initial={{ opacity: 0, scale: 0.5 }}
+    animate={{ opacity: 1, scale: 1 }}
+    transition={{ duration: 0.25 }}>
       <div className="w-[300px] bg-payne-gray text-peach py-4 px-2 mt-4 rounded-xl">
         <div className="text-center bg-moonstone mx-2 py-2 rounded-lg">
           <div className="font-semibold text-[1.5em]">
@@ -9,6 +14,6 @@ export default function ShortURL({ children }) {
           <div>{children}</div>
         </div>
       </div>
-    </>
+    </motion.div>
   );
 }
