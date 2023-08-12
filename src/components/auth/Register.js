a09a54f5059a50864e5069dad969e9efc28b2a18
Julian Lechner <jll38@njit.edu>
2023-08-12 14:48:25 +0200

    password match and strength

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,35 +1,51 @@
+import { PasswordStrengthBar } from "./PasswordStrengthBar";
 import { useState } from "react";
 import { passwordStrength as passStrength } from "check-password-strength";
 export default function Register() {
   const [password, setPassword] = useState(null);
   const [passwordStrength, setPasswordStrength] = useState({});
-
+  const [confirmPassword, setConfirmPassword] = useState(null);
   function passwordCheck(event) {
     setPassword(event.target.value);
     const strengthID = passStrength(password).id;
     let message;
+    let color;
     switch (strengthID) {
       case 0:
         message = "Pathetic 😰";
+        color = "bg-red-600";
         break;
       case 1:
         message = "Weak 😒";
+        color = "bg-orange-600";
         break;
       case 2:
         message = "Its ok... 🙄";
+        color = "bg-yellow-600";
         break;
       case 3:
         message = "Strong 💪";
+        color = "bg-green-600";
         break;
     }
     setPasswordStrength({
       id: strengthID,
       message: message,
+      color: color,
     });
+    console.log(strengthID)
   }
 
+  function confirmPasswordHandler(event){
+    const confirm = event.target.value;
+    setConfirmPassword(confirm === password)
+  }
   return (
-    <form id="loginForm" autoComplete="off" className="w-full authForm">
+    <form
+      id="loginForm"
+      autoComplete="off"
+      className="w-full authForm text-payne-gray"
+    >
       <label htmlFor="usernameInput" className="block">
         Username
       </label>
@@ -53,19 +69,53 @@ export default function Register() {
         id="passwordInput"
         type="password"
         className="block bg-white/50 rounded-lg"
-        onChange={
-          passwordCheck
-        }
+        onChange={passwordCheck}
       ></input>
-      <span class="help-block">{passwordStrength.message}</span>
-      <label htmlFor="confirmPasswordInput" className="block">
+      <div name="password-strength" className="text-sm -mt-4 mb-4">
+        Password Strength:
+        <div name="strength-bar" className="flex gap-2">
+          <div
+            className={`h-[5px] w-[40px] ${
+              passwordStrength.id >= 0 && password !== null
+                ? passwordStrength.color
+                : "bg-payne-gray"
+            }`}
+          ></div>
+          <div
+            className={`h-[5px] w-[40px] ${
+              passwordStrength.id >= 1 && password !== null
+                ? passwordStrength.color
+                : "bg-payne-gray"
+            }`}
+          ></div>
+          <div
+            className={`h-[5px] w-[40px] ${
+              passwordStrength.id >=2 && password !== null
+                ? passwordStrength.color
+                : "bg-payne-gray"
+            }`}
+          ></div>
+          <div
+            className={`h-[5px] w-[40px] ${
+              passwordStrength.id >= 3 && password !== null
+                ? passwordStrength.color
+                : "bg-payne-gray"
+            }`}
+          ></div>
+        </div>
+        <span class="help-block">{passwordStrength.message}</span>
+      </div>
+      <label htmlFor="confirmPasswordInput" className="block ">
         Confirm Password
       </label>
       <input
         id="confirmPasswordInput"
         type="password"
         className="block bg-white/50 rounded-lg"
+        onChange={confirmPasswordHandler}
       ></input>
+      {confirmPassword && <span>{confirmPassword ? ("Matching!") : ("Password does not match")}</span> //Work on this
+      }
       <button
         type="submit"
         className="bg-payne-gray text-white p-4 rounded-lg hover:bg-delft-blue transition-all duration-100"
