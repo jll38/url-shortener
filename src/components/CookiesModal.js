24982fba2c80fc4c1bae56f8f267200a5f1f1a58
Julian Lechner <jll38@njit.edu>
2023-12-08 14:14:28 -0500

    cookies modal

---
+++ src/components/CookiesModal.js
@@ -0,0 +1,51 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { Modal, Sheet, Typography, Button } from "@mui/joy";
+import { usePathname } from 'next/navigation'
+import { cookiesPages } from "@/lib/constants";
+export default function CookiesModal() {
+  const [allowCookies, setAllowCookies] = useState(false);
+  const pathname = usePathname();
+  useEffect(() => {
+    const storedAllowCookies = localStorage.getItem("allowCookies") === "true";
+    setAllowCookies(storedAllowCookies);
+  }, []);
+
+  const handleCookieAccept = () => {
+    localStorage.setItem("allowCookies", "true");
+    setAllowCookies(true);
+  };
+  console.log(cookiesPages);
+  if (!allowCookies && cookiesPages.includes(pathname)) {
+    return (
+      <Modal
+        open={!allowCookies}
+        sx={{ display: "flex", justifyContent: "center", alignItems: "center" }}
+      >
+        <Sheet
+          variant="outlined"
+          sx={{
+            maxWidth: 500,
+            borderRadius: "md",
+            p: 3,
+            boxShadow: "lg",
+          }}
+        >
+          <Typography sx={{ fontWeight: "600" }}>Cookies Notice</Typography>
+          <Typography>
+            By using this site, you accept our use of cookies
+          </Typography>
+          <Button
+            onClick={handleCookieAccept}
+            variant="outlined"
+            sx={{marginTop: "10px"}}
+          >
+            I accept
+          </Button>
+        </Sheet>
+      </Modal>
+    );
+  }
+
+  return null;
+}
