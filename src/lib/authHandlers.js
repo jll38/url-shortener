85b3ca27ed7ced50bad19f141349207a9f358bed
Julian Lechner <jll38@njit.edu>
2023-12-13 23:18:18 -0500

    fix hydration and localstorage access

--- src/lib/authHandlers.js
+++ src/lib/authHandlers.js
@@ -1,12 +1,20 @@
 import jwt from 'jsonwebtoken';
 
-export const user = jwt.decode(localStorage.getItem("JWT_X_AUTH"));
+export function getUser() {
+  if (typeof window !== 'undefined') {
+    const token = localStorage.getItem("JWT_X_AUTH");
+    return token ? jwt.decode(token) : null;
+  }
+  return null;
+}
 
-export function handleSignout(){
-    try{
-        localStorage.removeItem("JWT_X_AUTH");
-        window.location.reload();
-    } catch(e){
-        console.error("Error signing out. Might wanna fix that")
+export function handleSignout() {
+    if (typeof window !== 'undefined') {
+        try {
+            localStorage.removeItem("JWT_X_AUTH");
+            window.location.reload();
+        } catch(e) {
+            console.error("Error signing out. Might wanna fix that");
+        }
     }
 }
