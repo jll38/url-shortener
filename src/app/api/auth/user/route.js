bdf056cbdbfa551b8daa7938bcc07166ec816294
Julian Lechner <jll38@njit.edu>
2023-12-13 22:09:57 -0500

    test

--- src/app/api/auth/user/route.js
+++ src/app/api/auth/user/route.js
@@ -1,16 +1,7 @@
 // app/routes/api/protected.js
-import authMiddleware from '../../middleware/middleware';
 
 export async function POST({ request }) {
-  const authResult = authMiddleware(request);
-  if (authResult instanceof Response) {
-    return authResult; // If auth failed, it returns a Response object
-  }
-
-  // Continue with your logic for the authenticated user
-  // If the middleware did not return, it means the token is valid
-  const userData = { /* ... */ }; // Fetch user data based on `request.user` set by middleware
-
+  
   return new Response(JSON.stringify(userData), {
     status: 200,
     headers: { 'Content-Type': 'application/json' },
