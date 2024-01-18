64b1d0ce771a859dc5835e3f18de9f4904b2daa9
Julian Lechner <jll38@njit.edu>
2024-01-18 10:39:52 -0500

    chore: link portal dashboard editable pfp

---
+++ src/app/dashboard/link-portal/page.js
@@ -0,0 +1,115 @@
+"use client";
+import React from "react";
+
+import { Table, Sheet, Typography, Skeleton } from "@mui/joy";
+
+import Link from "next/link";
+import { CircularProgress } from "@mui/joy";
+import Tooltip from "@mui/joy/Tooltip";
+
+import { getUser } from "@/lib/authHandlers";
+import { useState, useEffect } from "react";
+
+import { ProfilePicture } from "./../../../components/ProfilePicture";
+
+export default function Geography() {
+  const [data, setData] = useState(null);
+  const [isEmptyData, setIsEmptyData] = useState(null);
+  const [user, setUser] = useState(null);
+  const [userLoading, setUserLoading] = useState(true);
+
+  const [unsavedChanges, setSavedChanges] = useState(false);
+  const [changes, setChanges] = useState(false);
+
+  async function saveChanges() {
+    setSavedChanges(false);
+    //Add backend query
+  }
+
+  async function assignUser() {
+    const userData = getUser(); // Fetch user data
+    setUser(userData); // Set user data to state
+    return userData; // Return the user data
+  }
+
+  const fetchData = async () => {
+    const assignedUser = await assignUser();
+    if (assignedUser) {
+      fetch(`/api/dash`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: assignedUser.id,
+        }),
+      })
+        .then((res) => {
+          if (!res.ok) {
+            throw new Error(res.statusText);
+          }
+          return res.json();
+        })
+        .then((data) => {
+          if (data.error) {
+            setIsEmptyData(true);
+          }
+          setData(data);
+        })
+        .catch((error) => {
+          console.error("Error:", error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    const loggedInUser = getUser();
+
+    if (loggedInUser) {
+      setUser(loggedInUser);
+      fetchData();
+
+      // // Call the API every X seconds
+      // const interval = setInterval(fetchData, DASHBOARD_FETCH_INTERVAL * 1000); // replace X with your interval in seconds
+
+      // Clean up the interval on component unmount
+      //return () => clearInterval(interval);
+    } else {
+      window.location.assign("/login");
+    }
+  }, []);
+
+  function getTime(zTime) {
+    const date = new Date(zTime);
+
+    return `${date.toLocaleTimeString("en-US", {
+      hour12: true,
+      hour: "numeric",
+      minute: "numeric",
+    })}`;
+  }
+
+  function getDate(zTime) {
+    const date = new Date(zTime);
+    return `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}`;
+  }
+
+  return (
+    <main className="h-screen overflow-x-hidden w-full">
+      {!data && (
+        <div className="h-screen w-full flex justify-center items-center">
+          <CircularProgress size="lg" />
+        </div>
+      )}
+      {data && (
+        <>
+          <section className="h-full relative w-full flex flex-col items-center">
+            <ProfilePicture editable={user ? true : false} />
+            <p>description</p>
+            <button onClick={saveChanges}></button>
+          </section>
+        </>
+      )}
+    </main>
+  );
+}
---
+++ src/components/ProfilePicture.js
@@ -0,0 +1,47 @@
+"use client";
+import React from "react";
+import EditIcon from "@mui/icons-material/Edit";
+export function ProfilePicture({ editable = false }) {
+  const [showEditButton, setShowEditButton] = React.useState(false);
+
+  const fileInputRef = React.useRef(null);
+
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    console.log(file);
+    // Handle the file...
+  };
+
+  return (
+    <div
+      className="h-[100px] w-[100px] grid place-items-center bg-gray-100  rounded-[10em] overflow-hidden"
+      onMouseEnter={() => {
+        if (editable) setShowEditButton(true);
+      }}
+      onMouseLeave={() => {
+        if (editable) setShowEditButton(false);
+      }}
+    >
+      {showEditButton ? (
+        <>
+          <input
+            type="file"
+            ref={fileInputRef}
+            onChange={handleFileChange}
+            style={{ display: "none" }}
+            accept="image/*"
+          />
+          <button className="w-full h-full hover:bg-gray-200" onClick={handleButtonClick}>
+            <EditIcon></EditIcon>{" "}
+          </button>
+        </>
+      ) : (
+        "test"
+      )}
+    </div>
+  );
+}
